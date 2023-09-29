ea56eee1e2ad3bc6a65065ccdd37e7aa843a67b3
Lucy kang <khjkgy1205@naver.com>
2023-09-29 09:22:36 +0900

    clock3

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  // getHours() 같은 것엔 padStart를 붙일 수 없다.
+  // getHours()는 숫자형이라 padStart를 붙일 수 없다.
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
